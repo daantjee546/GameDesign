1716502a110b52e4dc0d899f83a11db891c28380
stefan1234567891011 <s.hobeijn@hotmail.com>
2020-03-06 14:42:48 +0100

    profile added

---
+++ dungeon crawler/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    //public float speed;
+
+    //private Rigidbody2D rb;
+
+    private void Start()
+    {
+        //rb = GetComponent<Rigidbody2D>();
+        HealthBarHandlerEnemy.SetHealthBarValue(1);
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x -= 0.1f;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x += 0.1f;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y += 0.1f;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y -= 0.1f;
+            this.transform.position = position;
+        }
+    }
+
+    /*
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+    */
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        HealthBarHandlerEnemy.SetHealthBarValue(HealthBarHandlerEnemy.GetHealthBarValue() - 0.1f);
+        if (HealthBarHandlerEnemy.GetHealthBarValue() <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ dungeon crawler/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //public float speed;
+
+    //private Rigidbody2D rb;
+
+    private void Start()
+    {
+        //rb = GetComponent<Rigidbody2D>();
+        HealthBarHandler.SetHealthBarValue(1);
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 position = this.transform.position;
+            position.x -= 0.1f;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 position = this.transform.position;
+            position.x += 0.1f;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 position = this.transform.position;
+            position.y += 0.1f;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Vector3 position = this.transform.position;
+            position.y -= 0.1f;
+            this.transform.position = position;
+        }
+    }
+
+    /*
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+    */
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        HealthBarHandler.SetHealthBarValue(HealthBarHandler.GetHealthBarValue() - 0.1f);
+        if (HealthBarHandler.GetHealthBarValue() <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
