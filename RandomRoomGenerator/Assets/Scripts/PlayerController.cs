f22dd5e44dfbad74ec9755da3d60e078ec541645
daantjee546 <50410354+daantjee546@users.noreply.github.com>
2020-04-03 16:00:54 +0200

    added healthbar player, restart button, image when you die

--- RandomRoomGenerator/Assets/Scripts/PlayerController.cs
+++ RandomRoomGenerator/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     private Rigidbody2D rb;
+
+    public Image youDied;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        HealthBarHandler.SetHealthBarValue(1);
     }
 
     // Update is called once per frame
@@ -18,5 +23,19 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal") * moveSpeed;
         float moveVertical = Input.GetAxis("Vertical") * moveSpeed;
         rb.velocity = new Vector2(moveHorizontal, moveVertical);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            HealthBarHandler.SetHealthBarValue(HealthBarHandler.GetHealthBarValue() - 0.1f);
+        }
+        
+        if (HealthBarHandler.GetHealthBarValue() <= 0)
+        {
+            youDied.enabled = true;
+            Destroy(gameObject);
+        }
     }
 }
